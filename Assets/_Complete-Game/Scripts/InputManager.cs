7c77408abf5850a8f20c4dcabf963cee501fc5f4
ddayin <ddayin@hotmail.com>
2017-07-02 08:47:42 +0900

    moving directory

---
+++ Assets/_Complete-Game/Scripts/InputManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+    
+
+    private void Update()
+    {
+
+    }
+}
