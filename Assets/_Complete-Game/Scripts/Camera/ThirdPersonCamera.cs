b0a847111e3e029a4c8561c63749861221dab618
ddayin <ddayin@hotmail.com>
2017-07-02 14:11:01 +0900

    1. Fixing camera rotation around player 2. Added mouse sensitivity for camera

--- Assets/_Complete-Game/Scripts/Camera/ThirdPersonCamera.cs
+++ Assets/_Complete-Game/Scripts/Camera/ThirdPersonCamera.cs
@@ -20,6 +20,9 @@ public class ThirdPersonCamera : MonoBehaviour
     private float cameraHeight = 4.0f;
     private float currentX = 0.0f;
     private float currentY = 0.0f;
+    public float sensitivityMouseX = 5.0f;  // 마우스 민감도
+    public float sensitivityMouseY = 5.0f;  // 마우스 민감도
+    private Vector3 cameraPosition = new Vector3( 0, 1, -16 );
 
     private void Start()
     {
@@ -29,18 +32,20 @@ public class ThirdPersonCamera : MonoBehaviour
 
     private void Update()
     {
-        currentX += Input.GetAxis( "Mouse X" );
-        currentY += Input.GetAxis( "Mouse Y" );
+        currentX = currentX + Input.GetAxis( "Mouse X" ) * sensitivityMouseX;
+        currentY = currentY + Input.GetAxis( "Mouse Y" ) * sensitivityMouseY;
+
+        //Debug.Log( "mouse X = " + currentX );
+        //Debug.Log( "mouse Y = " + currentY );
 
         // mouse Y 값에 제한을 둬서 바닥 넘어서 보이지 않도록 Y 값 조정
-        currentY = Mathf.Clamp( currentY, Y_ANGLE_MIN, Y_ANGLE_MAX );
+        //currentY = Mathf.Clamp( currentY, Y_ANGLE_MIN, Y_ANGLE_MAX );
     }
 
     private void LateUpdate()
     {
-        Vector3 dir = new Vector3( 0, cameraHeight, -distance );
         Quaternion rotation = Quaternion.Euler( currentY, currentX, 0 );
-        cameraTransform.position = lookAt.position + rotation * dir;
+        cameraTransform.position = lookAt.position + rotation * cameraPosition;
 
         cameraTransform.LookAt( lookAt.position );
     }
