a7f3b9d16558124dc942232ac971498cd8443186
ddayin <ddayin@hotmail.com>
2017-07-02 10:53:51 +0900

    testing

---
+++ Assets/_Complete-Game/Scripts/Player/UserInput.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * https://youtu.be/gN1BmP4ONSQ
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
