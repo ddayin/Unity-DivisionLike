0c73d84de73596d7ad9f515a165e1cacb53b82e2
ddayin <ddayin@hotmail.com>
2017-07-05 03:23:46 +0900

    reorganizing directory

---
+++ Assets/Editor/CameraRigEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor( typeof( CameraControl ) )]
+public class CameraRigEditor : Editor
+{
+
+    CameraControl cameraRig;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        cameraRig = (CameraControl) target;
+
+        EditorGUILayout.LabelField( "Camera Helper" );
+
+        if ( GUILayout.Button( "Save camera's position now." ) )
+        {
+            Camera cam = Camera.main;
+
+            if ( cam )
+            {
+                Transform camT = cam.transform;
+                Vector3 camPos = camT.localPosition;
+                Vector3 camRight = camPos;
+                Vector3 camLeft = camPos;
+                camLeft.x = -camPos.x;
+                cameraRig.cameraSettings.camPositionOffsetRight = camRight;
+                cameraRig.cameraSettings.camPositionOffsetLeft = camLeft;
+            }
+        }
+    }
+}
---
+++ Assets/Editor/WeaponEditor.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor( typeof( Weapon ) )]
+public class WeaponEditor : Editor
+{
+    Weapon weapon;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        weapon = (Weapon) target;
+
+        EditorGUILayout.LabelField( "Weapon Helpers" );
+
+        if ( GUILayout.Button( "Save gun equip location." ) )
+        {
+            Transform weaponT = weapon.transform;
+            Vector3 weaponPos = weaponT.localPosition;
+            Vector3 weaponRot = weaponT.localEulerAngles;
+            weapon.weaponSettings.equipPosition = weaponPos;
+            weapon.weaponSettings.equipRotation = weaponRot;
+        }
+
+        if ( GUILayout.Button( "Save gun unequip location." ) )
+        {
+            Transform weaponT = weapon.transform;
+            Vector3 weaponPos = weaponT.localPosition;
+            Vector3 weaponRot = weaponT.localEulerAngles;
+            weapon.weaponSettings.unequipPosition = weaponPos;
+            weapon.weaponSettings.unequipRotation = weaponRot;
+        }
+
+        EditorGUILayout.LabelField( "Debug Positioning" );
+
+        if ( GUILayout.Button( "Move gun to equip location" ) )
+        {
+            Transform weaponT = weapon.transform;
+            weaponT.localPosition = weapon.weaponSettings.equipPosition;
+            Quaternion eulerAngles = Quaternion.Euler( weapon.weaponSettings.equipRotation );
+            weaponT.localRotation = eulerAngles;
+        }
+
+        if ( GUILayout.Button( "Move gun to unequip location" ) )
+        {
+            Transform weaponT = weapon.transform;
+            weaponT.localPosition = weapon.weaponSettings.unequipPosition;
+            Quaternion eulerAngles = Quaternion.Euler( weapon.weaponSettings.unequipRotation );
+            weaponT.localRotation = eulerAngles;
+        }
+    }
+}
