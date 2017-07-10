b348856491e049e8f26fb408b61390a077c203d1
ddayin <ddayin@hotmail.com>
2017-07-10 09:59:18 +0900

    added MiniMap system from https://github.com/Area730/MiniMap

---
+++ Assets/MiniMap/Scripts/InnerMap.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * reference - https://github.com/Area730/MiniMap
+ */ 
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[AddComponentMenu("MiniMap/Inner map")]
+[RequireComponent(typeof(Image))]
+public class InnerMap : MonoBehaviour {
+
+    private RectTransform _innerMapRect;
+
+    public RectTransform InnerMapRect
+    {
+        get
+        {
+            if (!_innerMapRect)
+            {
+                _innerMapRect = GetComponent<RectTransform>();
+            }
+
+            return _innerMapRect;
+        }
+    }
+
+    public float getMapRadius()
+    {
+        Vector3[] arr = new Vector3[4];
+        InnerMapRect.GetLocalCorners(arr);
+
+        float mapRadius;
+
+        if (Mathf.Abs(arr[0].x) < Mathf.Abs(arr[0].y))
+        {
+            mapRadius = Mathf.Abs(arr[0].x);
+        }
+        else
+        {
+            mapRadius = Mathf.Abs(arr[0].y);
+        }
+
+        return mapRadius;
+    }
+
+}
---
+++ Assets/MiniMap/Scripts/MarkerGroup.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * reference - https://github.com/Area730/MiniMap
+ */
+
+using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("MiniMap/Marker Group")]
+[RequireComponent(typeof(RectTransform))]
+public class MarkerGroup : MonoBehaviour {
+
+    public RectTransform MarkerGroupRect
+    {
+        get
+        {
+            if (!_rectTransform)
+            {
+                _rectTransform = GetComponent<RectTransform>();
+            }
+
+            return _rectTransform;
+        }
+    }
+
+    private RectTransform _rectTransform;
+
+}
