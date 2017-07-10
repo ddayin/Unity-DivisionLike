5aacf06655896ddf5b3d0cba29e3c5c5cf758421
ddayin <ddayin@hotmail.com>
2017-07-11 03:08:26 +0900

    added ammo slider next to crosshair

--- Assets/Scripts/GUI/ScreenHUD.cs
+++ Assets/Scripts/GUI/ScreenHUD.cs
@@ -10,7 +10,8 @@ namespace DivisionLike
         public static ScreenHUD instance = null;
 
         private Text levelText;
-        private Slider expSlider;
+        private Slider xpSlider;
+        private Slider ammoSlider;
 
         void Awake()
         {
@@ -26,10 +27,12 @@ namespace DivisionLike
             }
 
             levelText = transform.Find( "LevelText" ).GetComponent<Text>();
-            expSlider = transform.Find( "ExpSlider" ).GetComponent<Slider>();
-
+            xpSlider = transform.Find( "ExpSlider" ).GetComponent<Slider>();
+            ammoSlider = transform.Find( "AmmoSlider" ).GetComponent<Slider>();
+            
             SetLevelText();
             CalculateExpSlider( 0 );
+            SetAmmoSlider();
         }
 
         public void SetLevelText()
@@ -46,13 +49,19 @@ namespace DivisionLike
             float normalizedXP = (float) (Player.instance.stats.xp) / (float) (Player.instance.stats.xpRequire[ Player.instance.stats.level - 1 ]);
             //Debug.Log( "player xp = " + Player.instance.stats.xp );
             //Debug.Log( "normalized xp = " + normalizedXP );
-            expSlider.normalizedValue = normalizedXP;
+            xpSlider.normalizedValue = normalizedXP;
+        }
+
+        private void SetAmmoSlider()
+        {
+            float normalizedAmmo = (float) (Player.instance.weaponHandler.currentWeapon.ammo.clipAmmo) / (float) (Player.instance.weaponHandler.currentWeapon.ammo.maxClipAmmo);
+            ammoSlider.normalizedValue = normalizedAmmo;
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            SetAmmoSlider();
         }
     }
 }
