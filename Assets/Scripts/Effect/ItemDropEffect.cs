5e53836b5c162df282334234148ec2fee9b3a529
ddayin <ddayin@hotmail.com>
2017-07-13 05:59:17 +0900

    added line renderer on the dropped item

---
+++ Assets/Scripts/Effect/ItemDropEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DivisionLike
+{
+    public class ItemDropEffect : MonoBehaviour
+    {
+        private LineRenderer lineUp;
+
+        private void Awake()
+        {
+            lineUp = transform.GetComponent<LineRenderer>();
+
+            lineUp.SetPosition( 0, transform.position );
+
+            Vector3 onePosition = transform.position;
+            onePosition.y = 100f;
+            lineUp.SetPosition( 1, onePosition );
+        }
+    }
+}
--- Assets/Scripts/Managers/EffectManager.cs
+++ Assets/Scripts/Managers/EffectManager.cs
@@ -2,29 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EffectManager : MonoBehaviour
+
+namespace DivisionLike
 {
-    public static EffectManager instance = null;
-    public GameObject[] particlePrefabs = new GameObject[ 8 ];
-    public Transform particleParent;
-
-    private Dictionary<int, ParticleSystem> effectDic = new Dictionary<int, ParticleSystem>();
-
-    private void Awake()
+    public class EffectManager : MonoBehaviour
     {
-        instance = this;
-    }
+        public static EffectManager instance = null;
+        public GameObject[] particlePrefabs = new GameObject[ 8 ];
+        public Transform particleParent;
 
-    public void CreateParticle( int index, Vector3 position )
-    {
-        GameObject obj = Instantiate( particlePrefabs[ index ], position, Quaternion.identity, particleParent );
-        ParticleSystem particle = obj.transform.Find( "Particle System" ).GetComponent<ParticleSystem>();
+        private Dictionary<int, ParticleSystem> effectDic = new Dictionary<int, ParticleSystem>();
 
-        effectDic.Add( index, particle );
-    }
+        private void Awake()
+        {
+            instance = this;
+        }
 
-    public ParticleSystem GetParticle( int index )
-    {
-        return effectDic[ index ];
+        public void CreateParticle( int index, Vector3 position )
+        {
+            GameObject obj = Instantiate( particlePrefabs[ index ], position, Quaternion.identity, particleParent );
+            ParticleSystem particle = obj.transform.Find( "Particle System" ).GetComponent<ParticleSystem>();
+
+            effectDic.Add( index, particle );
+        }
+
+        public ParticleSystem GetParticle( int index )
+        {
+            return effectDic[ index ];
+        }
     }
 }
