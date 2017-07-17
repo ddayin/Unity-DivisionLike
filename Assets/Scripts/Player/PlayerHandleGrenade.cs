4be84e3382e3139db0dd8e46eb11e86d7a6eab51
ddayin <ddayin@hotmail.com>
2017-07-18 08:55:01 +0900

    grenade

---
+++ Assets/Scripts/Player/PlayerHandleGrenade.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DivisionLike
+{
+    public class PlayerHandleGrenade : MonoBehaviour
+    {
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
