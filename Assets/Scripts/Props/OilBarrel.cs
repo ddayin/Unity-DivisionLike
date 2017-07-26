d49c09022cefff6428f8cf5133c8ff28a95d84de
ddayin <ddayin@hotmail.com>
2017-07-26 16:14:41 +0900

    adding oil barrel system

---
+++ Assets/Scripts/Props/OilBarrel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DivisionLike
+{
+    public class OilBarrel : MonoBehaviour
+    {
+        public int _health = 100;
+
+        private void Awake()
+        {
+            
+        }
+        
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void Explode()
+        {
+            Destroy( this.gameObject, 2f );
+        }
+    }
+}
+
+
