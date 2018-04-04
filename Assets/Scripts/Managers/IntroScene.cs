63679101a9b05bfec069f6bbf22e1df7669a1fa4
DESKTOP-53O2T3R\ddayin <ddayin@gmail.com>
2018-04-04 14:08:48 +0900

    - 폴더 정리

---
+++ Assets/Scripts/Managers/IntroScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DivisionLike
+{
+    public class IntroScene : MonoBehaviour
+    {
+        private void Update()
+        {
+            ProcessKeyInput();
+        }
+
+        private void ProcessKeyInput()
+        {
+            if ( Input.GetKeyDown( KeyCode.Escape ) == true )
+            {
+#if UNITY_EDITOR
+
+#else
+                Application.Quit();
+#endif
+            }
+
+            if ( Input.GetKeyDown( KeyCode.Space ) == true || Input.GetKeyDown( KeyCode.Return ) == true )
+            {
+                SceneController.instance.LoadScene( eSceneName.Play );
+            }
+        }
+    }
+}
