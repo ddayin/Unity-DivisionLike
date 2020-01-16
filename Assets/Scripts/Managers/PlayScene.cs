eba9177d02265c44e17a99e9c96b2932e68d4724
ddayin <ddayin@gmail.com>
2020-01-16 13:54:48 +0900

    인트로 씬에 눈 매터리얼 추가

--- Assets/Scripts/Managers/PlayScene.cs
+++ Assets/Scripts/Managers/PlayScene.cs
@@ -1,7 +1,7 @@
 ﻿/*
 MIT License
 
-Copyright (c) 2019 ddayin
+Copyright (c) 2020 ddayin
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
